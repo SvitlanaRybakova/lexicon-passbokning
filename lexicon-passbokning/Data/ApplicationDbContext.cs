7e4303398d24418df365487ddd43b84c5a0fc343
Svitlana Rybakova <rsv0011@gmail.com>
2024-11-13 11:56:14 +0100

    feat: add auth configuration for DbContext

--- lexicon-passbokning/Data/ApplicationDbContext.cs
+++ lexicon-passbokning/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
 ﻿using lexicon_passbokning.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace lexicon_passbokning.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    // the contex config to use the correct types for users and roles.
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole, string>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
