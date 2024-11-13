7bb4ac74b96e4141e132fd5ed8ac21504012de70
Svitlana Rybakova <rsv0011@gmail.com>
2024-11-13 21:37:27 +0100

    feat: db seeding based on roles

---
+++ lexicon-passbokning/Data/SeedData.cs
@@ -0,0 +1,78 @@
+﻿using lexicon_passbokning.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace lexicon_passbokning.Data
+{
+    public class SeedData
+    {
+        private static ApplicationDbContext context = default!;
+        private static RoleManager<IdentityRole> roleManager = default!;
+        private static UserManager<ApplicationUser> userManager = default!;
+
+
+        public static async Task Init(ApplicationDbContext _context, IServiceProvider services)
+        {
+            context = _context;
+
+            if (context.Roles.Any()) return;
+
+            roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+            userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+
+            var roleNames = new[] { "User", "Admin" };
+            var adminEmail = "admin@Gymbokning.se";
+            var userEmail = "user@user.com";
+
+            await AddRolesAsync(roleNames);
+
+            var admin = await AddAccountAsync(adminEmail, "Admin", "Adminsson", "PWadmin-123");
+            var user = await AddAccountAsync(userEmail, "User", "Usersson", "PWuser-123");
+
+            await AddUserToRoleAsync(admin, "Admin");
+            await AddUserToRoleAsync(user, "User");
+        }
+        private static async Task AddUserToRoleAsync(ApplicationUser user, string roleName)
+        {
+            if (!await userManager.IsInRoleAsync(user, roleName))
+            {
+                var result = await userManager.AddToRoleAsync(user, roleName);
+                if (!result.Succeeded) throw new Exception(string.Join("\n", result.Errors));
+            }
+        }
+        private static async Task AddRolesAsync(string[] roleNames)
+        {
+            foreach (var roleName in roleNames)
+            {
+                if (await roleManager.RoleExistsAsync(roleName)) continue;
+                var role = new IdentityRole { Name = roleName };
+                var result = await roleManager.CreateAsync(role);
+                if (!result.Succeeded) throw new Exception(string.Join("\n", result.Errors));
+            }
+        }
+        private static async Task<ApplicationUser> AddAccountAsync(string accountEmail, string fName, string lName, string pw)
+        {
+            var found = await userManager.FindByEmailAsync(accountEmail);
+            if (found != null) return null!;
+
+            var user = new ApplicationUser
+            {
+                UserName = accountEmail,
+                Email = accountEmail,
+                FirstName = fName,
+                LastName = lName,
+                EmailConfirmed = true
+            };
+
+            var result = await userManager.CreateAsync(user, pw);
+
+            if (!result.Succeeded)
+            {
+                Console.WriteLine($"Error during seeding: {string.Join("\n", result.Errors)}");
+                throw new Exception(string.Join("\n", result.Errors));
+               
+            }
+                
+            return user;
+        }
+    }
+}
---
+++ lexicon-passbokning/Extentions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using lexicon_passbokning.Data;
+
+namespace lexicon_passbokning.Extentions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static async Task<IApplicationBuilder> SeedDataAsync(this IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<ApplicationDbContext>();
+                try
+                {
+                    await SeedData.Init(context, services);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error during seeding: {ex.Message}");
+                    throw;
+                }
+            }
+            return app;
+        }
+    }
+}
