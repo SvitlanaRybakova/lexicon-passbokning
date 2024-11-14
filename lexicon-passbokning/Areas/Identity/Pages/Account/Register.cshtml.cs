df0554b0a4a098228b27d1d4afb06a3da83bb272
Svitlana Rybakova <rsv0011@gmail.com>
2024-11-14 18:04:05 +0100

    feat: change email to full user name in navbar using  _userManager.AddClaimAsync()

--- lexicon-passbokning/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ lexicon-passbokning/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -19,6 +19,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
+using System.Security.Claims;
 
 namespace lexicon_passbokning.Areas.Identity.Pages.Account
 {
@@ -134,6 +135,9 @@ namespace lexicon_passbokning.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
+                    var fullName = $"{user.FirstName} {user.LastName}";
+                    await _userManager.AddClaimAsync(user, new Claim("FullName", fullName));
+
 
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
